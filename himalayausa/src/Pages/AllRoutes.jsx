f600c081d93c03d5937f26d64a9d5ecaafe7ac63
veenasahu12 <veenasahusbp@gmail.com>
2022-07-21 01:19:10 +0530

    Revert "submit"

--- himalayausa/src/Pages/AllRoutes.jsx
+++ himalayausa/src/Pages/AllRoutes.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Shop from "./Veena/Products/Shop";
 
 
 const AllRoutes=()=>{
 
     return(
         <div>
-            <Routes>
-                <Route path="/shop" element={<Shop />}/>
-            </Routes>
+            
         </div>
     )
 }
-export default AllRoutes;
+export default AllRoutes

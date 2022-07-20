98d7871c69cb5d15e5bba1b025073a48746b879b
Shilajit <shilajit2014paul@gmail.com>
2022-07-20 23:40:34 +0530

    Navbar
    Navigation with routing

--- himalayausa/src/Pages/AllRoutes.jsx
+++ himalayausa/src/Pages/AllRoutes.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Navbar from "../Components/Shilajit/Navbar/Navbar";
 
 
 const AllRoutes=()=>{
 
     return(
         <div>
-            
+           <Navbar/> 
         </div>
     )
 }

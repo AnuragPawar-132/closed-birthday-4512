993bf52260ca7a43085977a14deab558f1afb382
veenasahu12 <veenasahusbp@gmail.com>
2022-07-21 14:16:12 +0530

    add shop path

--- himalayausa/src/Pages/AllRoutes.jsx
+++ himalayausa/src/Pages/AllRoutes.jsx
@@ -4,24 +4,18 @@ import Navbar from "../Components/Shilajit/Navbar/Navbar";
 
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./Narayan/Homepage";
+import Shop from "./Veena/Products/Shop";
 
+const AllRoutes = () => {
+  return (
+    <div>
+      <Navbar />
 
-
-
-const AllRoutes=()=>{
-
-    return(
-        <div>
-
-           <Navbar/> 
-
-            <Routes>
-                
-                <Route path="/" element={<Homepage/> } />
-                
-            </Routes>
-
-        </div>
-    )
-}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/Shop" element={<Shop />} />
+      </Routes>
+    </div>
+  );
+};
 export default AllRoutes;

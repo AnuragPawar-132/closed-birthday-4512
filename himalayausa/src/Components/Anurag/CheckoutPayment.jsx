18387dc7644f4ec5446bf3561a70866bf16ec0a8
AnuragPawar-132 <anuragpawar.me@gmail.com>
2022-07-22 22:48:36 +0530

    cart, about pages are completed

--- himalayausa/src/Components/Anurag/CheckoutPayment.jsx
+++ himalayausa/src/Components/Anurag/CheckoutPayment.jsx
@@ -1,78 +1,111 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Stack, HStack, VStack, Text, Box, Image, Button, Spacer, Link, Input } from '@chakra-ui/react';
 import styles from "./CheckoutPayment.module.css"
+import axios from "axios"
 
 const CheckoutPayment=()=>{
 
-    const cartData=[
-        {
-            id: 1,
-            image: "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/Whitening-Mint---Box-Tube_small.png?v=1597743896",
-            name: "Simply Mint Whitening Toothpaste",
-            price: 299,
-            quantity: 1
-        },
-        {
-            id: 2,
-            image: "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/HimalayaBambooToothpaste-Tube_Front_small.png?v=1613065864",
-            name: "Bamboo & Sea Salt Whitening Antiplaque Toothpaste",
-            price: 366,
-            quantity: 2
-        },
-        {
-            id: 3,
-            image: "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/Whitening-Mint---Box-Tube_small.png?v=1597743896",
-            name: "Simply Mint Whitening Toothpaste",
-            price: 299,
-            quantity: 1
-        },
-        {
-            id: 4,
-            image: "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/HimalayaBambooToothpaste-Tube_Front_small.png?v=1613065864",
-            name: "Bamboo & Sea Salt Whitening Antiplaque Toothpaste",
-            price: 366,
-            quantity: 2
+    const [cartData, setCartData] = useState([])
+    const [promoText, setPromoText]= useState("");
+
+    let total_price = 0;
+    let promo_price = 0;
+        
+        for(let i=0; i<cartData.length; i++){
+            total_price+= (cartData[i].Price * cartData[i].count);
         }
-    ]
-    console.log(cartData)
+     
+
+    const getchMineCart=()=>{
+         axios.get(`https://my-himalayausa-project.herokuapp.com/cart`)
+         .then((res)=>setCartData(res.data))
+         .catch((err)=>console.log(err))
+    }
+
+    
+    useEffect(()=>{
+        getchMineCart();
+    }, [total_price])
+
+    if(promoText === "NATURE20"){
+        promo_price= total_price * 0.2
+    }
+
+    console.log(promoText)
 
     return (
         <VStack w="100%" spacing="5%" >
 
             <VStack className={styles.cartBox_any}  >
                 {
-                    cartData.map(({id, price, name, image})=>{
+                    cartData.map(({id, Price, Name, Img, count})=>{
                         return <HStack justifyContent="space-around" w="100%" >
-                                <Box w="25%" ><Image boxSize="60px" src={image} /></Box>
-                                <Box w="50%" ><Text fontSize='sm' fontWeight="semibold" textAlign="left" >{name}</Text></Box>
-                                <Box w="25%" ><Text fontSize='sm' fontWeight="semibold" >$ {price}</Text></Box>
+                                <div className={styles.countDisplayBox} ><Image boxSize="60px" src={Img} /><Box w="17px" h="20px" bg="grey" borderRadius="50px" ><Text color="white" >{count}</Text></Box></div>
+                                <Box w="50%" ><Text fontSize='sm' fontWeight="semibold" textAlign="left" >{Name}</Text></Box>
+                                <Box w="25%" ><Text fontSize='sm' fontWeight="semibold" >$ {Price.toFixed(2)}</Text></Box>
                         </HStack>
                     })
                 }
             </VStack>
 
             <HStack w="90%" >
-                <Input bg="white" size='lg' placeholder="Enter Promocode" />
-                <Button bg="lightgrey" size='lg' >Apply</Button>
+                <Input bg="white" size='lg' placeholder="Enter Promocode" onChange={(e)=>setPromoText(e.target.value)} />
+                <Button bg="lightgrey" size='lg'>Apply</Button>
             </HStack>
 
             <HStack justifyContent="space-between" w="86%" borderTop="1px solid lightgrey" borderBottom="1px solid lightgrey" padding="2%"  >
                 <VStack>
                     <Text color="grey" fontSize='sm' >Subtotal</Text>
                     <Text color="grey" fontSize='sm' >Shipping</Text>
+                    <Text color="grey" fontSize='sm' >Discount</Text>
                 </VStack>
                 <VStack>
-                    <Text color="grey" fontSize='sm' >$ 877</Text>
-                    <Text color="grey" fontSize='sm' >$ 30</Text>
+                    <Text color="grey" fontSize='sm' >$ {total_price.toFixed(2)}</Text>
+                    <Text color="grey" fontSize='sm' >$ 5.00</Text>
+                    <Text color="grey" fontSize='sm' >$ {promo_price.toFixed(2)}</Text>
                 </VStack>
             </HStack>
 
             <HStack justifyContent="space-between" w="86%" borderTop="1px solid lightgrey" borderBottom="1px solid lightgrey" padding="4% 2% 4% 2%" >
                 <Box><Text fontSize='lg' >Total</Text></Box>
-                <Box><Text fontSize='lg' >$ 907</Text></Box>
+                <Box><Text fontSize='lg' >$ {(total_price + 5 - promo_price).toFixed(2)}</Text></Box>
             </HStack>
         </VStack>
     )
 }
 
 export default CheckoutPayment;
+
+
+
+
+// const cartData=[
+    //     {
+    //         id: 1,
+    //         image: "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/Whitening-Mint---Box-Tube_small.png?v=1597743896",
+    //         name: "Simply Mint Whitening Toothpaste",
+    //         price: 299,
+    //         quantity: 1
+    //     },
+    //     {
+    //         id: 2,
+    //         image: "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/HimalayaBambooToothpaste-Tube_Front_small.png?v=1613065864",
+    //         name: "Bamboo & Sea Salt Whitening Antiplaque Toothpaste",
+    //         price: 366,
+    //         quantity: 2
+    //     },
+    //     {
+    //         id: 3,
+    //         image: "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/Whitening-Mint---Box-Tube_small.png?v=1597743896",
+    //         name: "Simply Mint Whitening Toothpaste",
+    //         price: 299,
+    //         quantity: 1
+    //     },
+    //     {
+    //         id: 4,
+    //         image: "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/HimalayaBambooToothpaste-Tube_Front_small.png?v=1613065864",
+    //         name: "Bamboo & Sea Salt Whitening Antiplaque Toothpaste",
+    //         price: 366,
+    //         quantity: 2
+    //     }
+    // ]
---
+++ himalayausa/src/Pages/Anurag/Cart.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import { Stack, HStack, VStack, Text, Box, Image, Button, Spacer, Link, Input } from '@chakra-ui/react';
+import styles from "./Cart.module.css";
+import axios from "axios";
+
+const Cart=()=>{
+
+    const [cartData, setCartData]= useState([])
+
+    const fetchCartFunction=()=>{
+        axios.get(`https://my-himalayausa-project.herokuapp.com/cart`)
+        .then((res)=>setCartData(res.data))
+        .catch((err)=>console.log(err))
+    }
+
+    const handleCartItemDelete=(id)=>{
+        axios.delete(`https://my-himalayausa-project.herokuapp.com/cart/${id}`)
+    }
+
+    const handleChangeQuantity=(x, id, y)=>{
+        
+            axios.patch(`https://my-himalayausa-project.herokuapp.com/cart/${id}`,{
+            count : y + x
+        })
+        // .then((res)=>setCartData(res.data))
+        // .catch((err)=>console.log(err))
+        fetchCartFunction()
+    }
+
+    let total_price = 0;
+        
+        for(let i=0; i<cartData.length; i++){
+            total_price+= (cartData[i].Price * cartData[i].count);
+        }
+
+    useEffect(()=>{
+        fetchCartFunction()
+    },[cartData])
+
+    return (
+        <VStack padding="2%" w="100%" >
+            <Box>
+                <Text fontSize="4xl" fontWeight="semibold" >Shopping Cart</Text>
+            </Box>
+            <VStack w="100%" padding="3%"  >
+                {
+                    cartData.length>0 && cartData.map(({Img, Name, id, Price, count})=>{
+                        return(
+                            <div className={styles.cartBox_any} >
+                                <Button color="grey" bg="white" onClick={()=>handleCartItemDelete(id)} >🗑</Button>
+
+                                <Box bg="#f6f6f6" ><Image src={Img} /></Box>
+
+                                <Box fontSize="2xl" textAlign="left" ><Text>{Name}</Text></Box>
+
+                                <Box><Text fontSize="2xl" color="grey" >$ {Price.toFixed(2)}</Text></Box>
+
+                                <HStack>
+                                    <Button onClick={()=>{if(count>1){handleChangeQuantity(-1, id, count)}}} >-</Button>
+                                    <Text fontSize="2xl" color="grey">{count}</Text>
+                                    <Button onClick={()=>{if(count<4){handleChangeQuantity(+1, id, count)}}} >+</Button>
+                                </HStack>
+
+                                <Box><Text fontSize="2xl" color="grey" >$ {(Price * count).toFixed(2)}</Text></Box>
+                            </div>
+                        )
+                    })
+                }
+            </VStack>
+            <HStack bg="#f6f6f6" w="100%" padding="3%" justifyContent="space-between" >
+                <Box><Text fontSize="2xl" >Total Items : {cartData.length}</Text></Box>
+                <Box><Text fontSize="2xl" >Total Amount: $ {total_price.toFixed(2)}</Text></Box>
+                <Button size="lg" bg="#0D362B" color="white" >Proceed To Checkout</Button>
+            </HStack>
+        </VStack>
+    )
+}
+export default Cart

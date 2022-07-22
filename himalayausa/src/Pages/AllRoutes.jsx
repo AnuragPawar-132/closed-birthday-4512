e0258366762e42c2a0d5deddc461c3ee9d757b74
veenasahu12 <veenasahusbp@gmail.com>
2022-07-23 03:50:51 +0530

    create team

--- himalayausa/src/Pages/AllRoutes.jsx
+++ himalayausa/src/Pages/AllRoutes.jsx
@@ -1,39 +1,27 @@
-
-
 import React from "react";
-
 import Navbar from "../Components/Shilajit/Navbar/Navbar";
-
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./Narayan/Homepage";
 import Registration from "../Components/Shilajit/Reg/Registration";
 import Login from "../Components/Shilajit/Log/Login";
-
 import Shop from "./Veena/Products/Shop/Shop";
 import OralCare from "./Veena/Products/OralCare/OralCare";
 import OralCareDetails from "./Veena/Products/OralCare/OralCareDetails";
 import ShopDetails from "./Veena/Products/Shop/ShopDetails";
 
-
 const AllRoutes = () => {
   return (
     <div>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/Shop" element={<Shop />} />
-
         <Route path="/Registration" element={<Registration />} />
         <Route path="/Login" element={<Login />} />
-
-
+        <Route path="/Shop" element={<Shop />} />
         <Route path="/Shop/:id" element={<ShopDetails />} />
         <Route path="/OralCare" element={<OralCare />} />
-        <Route path="/OralCare/:id" element={<OralCareDetails />}/>
-
+        <Route path="/OralCare/:id" element={<OralCareDetails />} />
       </Routes>
-
     </div>
   );
 };
